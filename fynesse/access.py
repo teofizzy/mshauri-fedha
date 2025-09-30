875af6769e60136866e015ffc9280f3e06525ccb
teofizzy <teofilo48ligawa@gmail.com>
2025-09-30 18:56:39 +0000

    cbk explorer

--- fynesse/access.py
+++ fynesse/access.py
@@ -241,3 +241,16 @@ class CBKExplorer:
 
         return {"title": title, "anchors": anchors, "file_links": file_like, "class_counts": class_counts}
 
+import os, subprocess, importlib, sys
+
+def load_repo(repo):
+    local = repo.split("/")[-1]
+    if not os.path.exists(local):
+        subprocess.run(["git", "clone", f"https://github.com/{repo}.git"], check=True)
+    else:
+        subprocess.run(["git", "-C", local, "pull"], check=True)
+    if local not in sys.path:
+        sys.path.insert(0, local)
+    mod = importlib.import_module(local)
+    importlib.reload(mod)
+    return mod
